72ba36e092d6eb110d49133586fdb42b7654946e
adithyapradeepkumar <78904455+adithyapradeepkumar@users.noreply.github.com>
2021-02-11 15:22:31 +0530

    Add files via upload

---
+++ Free.java
@@ -0,0 +1,40 @@
+import java.io.Console;
+import java.util.Scanner;
+
+public class Free {
+	
+	//static double pie= 3.14;
+
+	public static void main(String[] args) 
+	
+	{
+			   //  Scanner in = new Scanner(System.in);
+			   //  System.out.print("Input a String: ");
+				// String word = in.nextLine();
+			 //word=word.trim();  
+			//	 System.out.println(word); 
+				// char Result;
+				// System.out.println();
+				 
+				int[] ch = {1,2,4,5,6};
+				
+				int[] ch1= {2,1,4,6,7,4,9};
+	
+				for(int i=0;i<ch.length;i++)
+				{
+					for(int j=0;j<ch1.length;j++)
+					{
+						if(ch[i]!=ch1[j])
+						{
+							System.out.print(ch[i]);
+						}
+					}
+					}
+
+				 
+				 
+				 
+				
+	}		
+	
+}		
